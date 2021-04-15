d508b1add22fa3f43525485028d955f79d1c58d9
8 <lucinda.souchet@outlook.com>
2021-04-15 21:50:17 +0200

    add Game component

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+// Game component
+const Game = () => {
+  return (
+    <div >
+      Game
+    </div>
+  );
+};
+
+// Game component render
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
