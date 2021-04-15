c34d422dc257408c7e974992c9c7c8873d01b5e6
8 <lucinda.souchet@outlook.com>
2021-04-15 22:08:07 +0200

    add Board and Square components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,45 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
+// Square component
+const Square = () => {
+  return (
+    <div style={{
+      backgroundColor: "DimGrey",
+      margin: 10,
+      padding: 20,
+    }}>
+      Square
+    </div>
+  );
+};
+
+
+// Board component
+const Board = () => {
+  return (
+    <div style={{
+      backgroundColor: "grey",
+      margin: 10,
+      padding: 20,
+    }}>
+      Board
+      <Square />
+    </div>
+  );
+};
+
+
 // Game component
 const Game = () => {
   return (
-    <div >
+    <div style={{
+      backgroundColor: "DarkGrey",
+      margin: 10,
+      padding: 20,
+    }}>
       Game
+      <Board />
     </div>
   );
 };
